53af3eb7ac3fb8dc051ae81a6cea8f35a4e1d111
shenzhili <17183379672@163.com>
2020-02-09 14:49:10 +0800

    18.4

---
+++ 18.4-sum.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=18 lang=cpp
+ *
+ * [18] 4Sum
+ */
+
+// @lc code=start
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        
+    }
+};
+// @lc code=end
+
--- test.cpp
+++ test.cpp
@@ -2,11 +2,12 @@
 using namespace std;
 
 int main(){
-    vector<int> a = {1, 2, 2, 3, 4, 45};
+    vector<int> nums = {1, 2, 2, 3, 4, 45};
 
-    auto i = a.end();
-    printf("%d", *prev(i, 2));
-    getchar();
-    return 0;
-    
+    map<int, vector<pair<int, int>>> cache;
+    for (auto a = nums.begin(); a < prev(nums.end()); a++){
+        for (auto b = a + 1; b < nums.end(); b++){
+            cache[*a + *b].push_back(pair<int, int>)
+        }
+    }
 }
